8980b2bb65d3641fee32e6ed17fc1161e710a54d
Pratik Jussal <pratikjaslofficial@gmail.com>
2025-02-28 11:36:48 +0530

    Links Added

--- client/src/components/sections/contact.tsx
+++ client/src/components/sections/contact.tsx
@@ -6,9 +6,15 @@ function Contact(){
         <section id="contact" className="flex flex-col gap-5 scroll-smooth lg:p-10 p-5 text-center shadow-2xl shadow-black w-full">
             
             <div className="flex flex-row gap-5 w-full justify-center">
-                <a href=""><i className="fab fa-instagram text-3xl text-red-500 hover:scale-150 active:scale-150 transition transform duration-500 ease-in-out"></i></a>
-                <a href=""><i className="fab fa-linkedin text-3xl text-blue-500 hover:scale-150 active:scale-150 transition transform duration-500 ease-in-out"></i></a>
-                <a href=""><i className="fab fa-youtube text-3xl text-red-500 hover:scale-150 active:scale-150 transition transform duration-500 ease-in-out"></i></a> 
+                <a href="https://www.instagram.com/ghoshgoes/" target="blank">
+                    <i className="fab fa-instagram text-3xl text-red-500 hover:scale-150 active:scale-150 transition transform duration-500 ease-in-out"></i>
+                </a>
+                <a href="https://www.linkedin.com/in/amrita-ghosh-53a203161/?original_referer=https%3A%2F%2Fwww%2Egoogle%2Ecom%2F&originalSubdomain=in" target="blank">
+                    <i className="fab fa-linkedin text-3xl text-blue-500 hover:scale-150 active:scale-150 transition transform duration-500 ease-in-out"></i>
+                </a>
+                <a href="https://www.youtube.com/watch?v=HmzcfFZrz90" target="blank">
+                    <i className="fab fa-youtube text-3xl text-red-500 hover:scale-150 active:scale-150 transition transform duration-500 ease-in-out"></i>
+                </a>    
                 <div className="flex items-center gap-2 hover:scale-120 active:scale-120 transition transform duration-500 ease-in-out">
                     <PhoneIcon className="h-6 w-6 text-black" />
                     <h3 className="text-lg">+91 1234567890</h3>
--- client/src/components/sections/home.tsx
+++ client/src/components/sections/home.tsx
@@ -30,9 +30,15 @@ function Home(){
                     </p>
 
                     <div className="flex flex-row gap-5 w-full justify-center">
-                        <a href=""><i className="fab fa-instagram text-3xl text-red-500 hover:scale-150 active:scale-150 transition transform duration-500 ease-in-out"></i></a>
-                        <a href=""><i className="fab fa-linkedin text-3xl text-blue-500 hover:scale-150 active:scale-150 transition transform duration-500 ease-in-out"></i></a>
-                        <a href=""><i className="fab fa-youtube text-3xl text-red-500 hover:scale-150 active:scale-150 transition transform duration-500 ease-in-out"></i></a>
+                        <a href="https://www.instagram.com/ghoshgoes/" target="blank">
+                            <i className="fab fa-instagram text-3xl text-red-500 hover:scale-150 active:scale-150 transition transform duration-500 ease-in-out"></i>
+                        </a>
+                        <a href="https://www.linkedin.com/in/amrita-ghosh-53a203161/?original_referer=https%3A%2F%2Fwww%2Egoogle%2Ecom%2F&originalSubdomain=in" target="blank">
+                            <i className="fab fa-linkedin text-3xl text-blue-500 hover:scale-150 active:scale-150 transition transform duration-500 ease-in-out"></i>
+                        </a>
+                        <a href="https://www.youtube.com/watch?v=HmzcfFZrz90" target="blank">
+                            <i className="fab fa-youtube text-3xl text-red-500 hover:scale-150 active:scale-150 transition transform duration-500 ease-in-out"></i>
+                        </a>
                         <div className="flex items-center gap-2 hover:scale-120 active:scale-120 transition transform duration-500 ease-in-out">
                             <PhoneIcon className="h-6 w-6 text-black" />
                             <h3 className="text-lg">+911234567890</h3>
