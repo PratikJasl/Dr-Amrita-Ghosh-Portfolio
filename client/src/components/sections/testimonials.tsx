487f6b6e3a203565b3a6848019cfd15d85d4f51f
Pratik Jussal <pratikjaslofficial@gmail.com>
2025-02-28 10:59:53 +0530

    Corrections in Testimonials

--- client/src/components/sections/testimonials.tsx
+++ client/src/components/sections/testimonials.tsx
@@ -9,8 +9,9 @@ function Testimonials(){
             <h2 className="text-2xl font-serif italic"> what our <span className="text-blue-500 text-3xl">patients say</span></h2>
 
             <div className="flex flex-row gap-10 m-10 w-full h-110 p-5 overflow-x-auto scrollbar-custom">
-                <div className="flex flex-col min-w-64 lg:flex-shrink-0 lg:w-90 justify-center items-center relative p-5 shadow-lg shadow-gray-400 rounded-xl text-center">
-                    <img src={profilePic} alt="person" className="rounded-full border-1 h-25 w-25 absolute top-5"/>
+
+                <div className="flex flex-col gap-3 justify-start min-w-64 lg:flex-shrink-0 lg:w-90 items-center relative p-5 shadow-lg shadow-gray-400 rounded-xl text-center">
+                    <img src={profilePic} alt="person" className="rounded-full border-1 h-25 w-25"/>
                     <p className="text-justify text-gray-600 font-serif">
                         We have visited many doctors but they were only changing one medicine to others with 
                         unsatisfactory results. Finally after Dr Amrita's treatment my dad's sugars are in control. 
@@ -19,8 +20,8 @@ function Testimonials(){
                     <img src={pattern1} alt="pattern-1" className="h-15 w-15 absolute bottom-5 right-5"/>
                 </div>
 
-                <div className="flex flex-col min-w-64 lg:flex-shrink-0 lg:w-90 justify-center items-center relative p-5 shadow-lg shadow-gray-400 rounded-xl text-center">
-                    <img src={profilePic} alt="person" className="rounded-full border-1 h-25 w-25 absolute top-5" />
+                <div className="flex flex-col gap-3 min-w-64 lg:flex-shrink-0 lg:w-90 justify-start items-center relative p-5 shadow-lg shadow-gray-400 rounded-xl text-center">
+                    <img src={profilePic} alt="person" className="rounded-full border-1 h-25 w-25" />
                     <p className="text-justify text-gray-600 font-serif">
                         One of the best doctor I have ever met. We met so many doctors earlier, 
                         but the way she has explained and treated my diseases no one did it earlier. 
@@ -29,8 +30,8 @@ function Testimonials(){
                     <img src={pattern1} alt="pattern-1" className="h-15 w-15 absolute bottom-5 right-5"/>
                 </div>
 
-                <div className="flex flex-col min-w-64 lg:flex-shrink-0 lg:w-90 justify-center items-center relative p-5 shadow-lg shadow-gray-400 rounded-xl text-center">
-                    <img src={profilePic} alt="person" className="rounded-full border-1 h-25 w-25 absolute top-5" />
+                <div className="flex flex-col gap-3 min-w-64 lg:flex-shrink-0 lg:w-90 justify-start items-center relative p-5 shadow-lg shadow-gray-400 rounded-xl text-center">
+                    <img src={profilePic} alt="person" className="rounded-full border-1 h-25 w-25" />
                     <p className="text-justify text-gray-600 font-serif">
                     Her diagnoses and holistic approach to remedy is great. Recommended treatment was very practical. In addition to medicines, 
                     simple changes to diet and lifestyle advice was very helpful. 
@@ -39,8 +40,8 @@ function Testimonials(){
                     <img src={pattern1} alt="pattern-1" className="h-15 w-15 absolute bottom-5 right-5"/>
                 </div>
 
-                <div className="flex flex-col min-w-64 lg:flex-shrink-0 lg:w-90 justify-center items-center relative p-5 shadow-lg shadow-gray-400 rounded-xl text-center">
-                    <img src={profilePic} alt="person" className="rounded-full border-1 h-25 w-25 absolute top-5"/>
+                <div className="flex flex-col gap-3 min-w-64 lg:flex-shrink-0 lg:w-90 justify-start items-center relative p-5 shadow-lg shadow-gray-400 rounded-xl text-center">
+                    <img src={profilePic} alt="person" className="rounded-full border-1 h-25 w-25"/>
                     <p className="text-justify text-gray-600 font-serif">
                     Her diagnoses and holistic approach to remedy is great. Recommended treatment was very practical. In addition to medicines, 
                     simple changes to diet and lifestyle advice was very helpful. 
@@ -49,8 +50,8 @@ function Testimonials(){
                     <img src={pattern1} alt="pattern-1" className="h-15 w-15 absolute bottom-5 right-5"/>
                 </div>
 
-                <div className="flex flex-col min-w-64 lg:flex-shrink-0 lg:w-90 justify-center items-center relative p-5 shadow-lg shadow-gray-400 rounded-xl text-center">
-                    <img src={profilePic} alt="person" className="rounded-full border-1 h-25 w-25 absolute top-5"/>
+                <div className="flex flex-col gap-3 min-w-64 lg:flex-shrink-0 lg:w-90 justify-start items-center relative p-5 shadow-lg shadow-gray-400 rounded-xl text-center">
+                    <img src={profilePic} alt="person" className="rounded-full border-1 h-25 w-25"/>
                     <p className="text-justify text-gray-600 font-serif">
                     Her diagnoses and holistic approach to remedy is great. Recommended treatment was very practical. In addition to medicines, 
                     simple changes to diet and lifestyle advice was very helpful. 
