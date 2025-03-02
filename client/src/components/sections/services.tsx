c0f7c3e0373e1186d639126f6113d5709a685479
Pratik Jussal <pratikjaslofficial@gmail.com>
2025-03-02 11:22:39 +0530

    improvements made to services section

--- client/src/components/sections/services.tsx
+++ client/src/components/sections/services.tsx
@@ -1,3 +1,9 @@
+import diabetesManage from "../../assets/dibetis-management.jpg";
+import dietPlan from "../../assets/diet-plan.jpg";
+import gestational from "../../assets/pregnant.jpg";
+import footCare from "../../assets/foot1.jpg";
+import glucose from "../../assets/glucose.jpg";
+import cardiology from "../../assets/cardiology.jpg";
 
 function Services(){
     return(
@@ -7,70 +13,53 @@ function Services(){
 
             <div className="flex flex-col flex-wrap justify-center lg:flex lg:flex-row gap-10 m-10 ">
 
-                <div className="shadow-lg shadow-gray-400 p-4 h-70 w-70 hover:scale-110 active:scale-110 transition transform duration-400 ease-in-out rounded-lg
-                text-center flex flex-col justify-center gap-5 items-center"
+                <div 
+                    className="shadow-lg shadow-gray-400 p-4 h-70 w-70 hover:scale-110 active:scale-110 transition transform duration-400 ease-in-out rounded-lg
+                    flex flex-col gap-5 items-center justify-end"
+                    style={{ backgroundImage: `url(${diabetesManage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
                 >
-                    <div className="text-xl font-bold font-serif">Diabetes Management</div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tempus nunc sit amet risus commodo aliquet et sed turpis.</p>
-                    {/* <button 
-                        className="bg-yellow-500 text-white text-lg p-2 w-fit rounded-lg hover:bg-amber-500">
-                        Book Appointemnt
-                    </button> */}
+                    <div className="text-xl font-bold font-serif text-white bg-amber-400 rounded-lg p-1">Diabetes Management</div>
                 </div>
 
-                <div className="shadow-lg shadow-gray-400 p-4 h-70 w-70 hover:scale-110 active:scale-110 transition transform duration-400 ease-in-out rounded-lg
-                text-center flex flex-col justify-center gap-5 items-center"
+                <div 
+                    className="shadow-lg shadow-gray-400 p-4 h-70 w-70 hover:scale-110 active:scale-110 transition transform duration-400 ease-in-out rounded-lg
+                    flex flex-col gap-5 items-center justify-end"
+                    style={{ backgroundImage: `url(${dietPlan})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
                 >
-                    <div className="text-xl font-bold font-serif">Diabetes Management</div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tempus nunc sit amet risus commodo aliquet et sed turpis.</p>
-                    {/* <button 
-                        className="bg-yellow-500 text-white text-lg p-2 w-fit rounded-lg hover:bg-amber-500">
-                        Book Appointemnt
-                    </button> */}
+                    <div className="text-xl font-bold font-serif text-white bg-amber-400 rounded-lg p-1">Diet Planning</div>
                 </div>
 
-                <div className="shadow-lg shadow-gray-400 p-4 h-70 w-70 hover:scale-110 active:scale-110 transition transform duration-400 ease-in-out rounded-lg
-                text-center flex flex-col justify-center gap-5 items-center"
+                <div 
+                    className="shadow-lg shadow-gray-400 p-4 h-70 w-70 hover:scale-110 active:scale-110 transition transform duration-400 ease-in-out rounded-lg
+                    flex flex-col gap-5 items-center justify-end"
+                    style={{ backgroundImage: `url(${gestational})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
                 >
-                    <div className="text-xl font-bold font-serif">Diabetes Management</div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tempus nunc sit amet risus commodo aliquet et sed turpis.</p>
-                    {/* <button 
-                        className="bg-yellow-500 text-white text-lg p-2 w-fit rounded-lg hover:bg-amber-500">
-                        Book Appointemnt
-                    </button> */}
+                    <div className="text-xl font-bold font-serif text-white bg-amber-400 rounded-lg p-1">Gestational Diabetes</div>
                 </div>
 
-                <div className="shadow-lg shadow-gray-400 p-4 h-70 w-70 hover:scale-110 active:scale-110 transition transform duration-400 ease-in-out rounded-lg
-                text-center flex flex-col justify-center gap-5 items-center"
+                <div 
+                    className="shadow-lg shadow-gray-400 p-4 h-70 w-70 hover:scale-110 active:scale-110 transition transform duration-400 ease-in-out rounded-lg
+                    flex flex-col gap-5 items-center justify-end"
+                    style={{ backgroundImage: `url(${footCare})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
                 >
-                    <div className="text-xl font-bold font-serif">Diabetes Management</div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tempus nunc sit amet risus commodo aliquet et sed turpis.</p>
-                    {/* <button 
-                        className="bg-yellow-500 text-white text-lg p-2 w-fit rounded-lg hover:bg-amber-500">
-                        Book Appointemnt
-                    </button> */}
+                    <div className="text-xl font-bold font-serif text-white bg-amber-400 rounded-lg p-1">Diabetic Foot Care</div>
+                    
                 </div>
 
-                <div className="shadow-lg shadow-gray-400 p-4 h-70 w-70 hover:scale-110 active:scale-110 transition transform duration-400 ease-in-out rounded-lg
-                text-center flex flex-col justify-center gap-5 items-center"
+                <div 
+                    className="shadow-lg shadow-gray-400 p-4 h-70 w-70 hover:scale-110 active:scale-110 transition transform duration-400 ease-in-out rounded-lg
+                    flex flex-col gap-5 items-center justify-end"
+                    style={{ backgroundImage: `url(${glucose})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
                 >
-                    <div className="text-xl font-bold font-serif">Diabetes Management</div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tempus nunc sit amet risus commodo aliquet et sed turpis.</p>
-                    {/* <button 
-                        className="bg-yellow-500 text-white text-lg p-2 w-fit rounded-lg hover:bg-amber-500">
-                        Book Appointemnt
-                    </button> */}
+                    <div className="text-xl font-bold font-serif text-white bg-amber-400 rounded-lg p-1">Glucose Intolerance</div>  
                 </div>
 
-                <div className="shadow-lg shadow-gray-400 p-4 h-70 w-70 hover:scale-110 active:scale-110 transition transform duration-400 ease-in-out rounded-lg
-                text-center flex flex-col justify-center gap-5 items-center"
+                <div 
+                    className="shadow-lg shadow-gray-400 p-4 h-70 w-70 hover:scale-110 active:scale-110 transition transform duration-400 ease-in-out rounded-lg
+                    flex flex-col gap-5 items-center justify-end"
+                    style={{ backgroundImage: `url(${cardiology})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
                 >
-                    <div className="text-xl font-bold font-serif">Diabetes Management</div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tempus nunc sit amet risus commodo aliquet et sed turpis.</p>
-                    {/* <button 
-                        className="bg-yellow-500 text-white text-lg p-2 w-fit rounded-lg hover:bg-amber-500">
-                        Book Appointemnt
-                    </button> */}
+                    <div className="text-xl font-bold font-serif text-white bg-amber-400 rounded-lg p-1">Clinical Cardiology</div>
                 </div>
 
             </div>
